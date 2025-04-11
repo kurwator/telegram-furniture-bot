3b3393de1788060e52663894bbe9cc3de4ff8eee
kurwator <achikyanart@gmail.com>
2025-04-11 19:03:08 +0300

    Исправлен путь к базе данных

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Токен бота Telegram
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("7467352806:AAHNd_kgcvuSDo5UkEFn53kNpJcRCke9DZo")
 
 # Путь к базе данных
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///data/catalog.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////data/catalog.db")
 
 # Секретный ключ для хеширования
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
